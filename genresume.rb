751188a125c533a7b417004a89071b1b1cd34b52
hywn <meu email>
2020-05-31 16:21:39 -0400

    wrote equivalentish ruby script

---
+++ genresume.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+text = File.read 'resume.txt'
+css  = File.read 'style.css'
+
+lines = text.lines.map(&:strip).filter {|line| not line.empty?}
+
+header, *rest = lines
+
+classes = header[1..-1].split('\\')
+
+trs = rest.map { |line|
+
+	parts = line.split('\\').each_with_index.map { |content, i|
+
+		content.strip!
+
+		content = "<h1>#{$1}</h1>"                 if content.match /#(.+)/
+		content = "<blockquote>#{$1}</blockquote>" if content.match /^> (.+)/
+
+		"<td class=\"#{classes[i]}\">#{content}</td>"
+
+	}
+
+	parts = ['<td></td>'] if parts.empty?
+
+	"<tr>#{parts.join ''}</tr>"
+
+}
+
+File.write 'index.html', "<style>#{css}</style><div id=\"wrapper\"><table>#{trs.join ''}</table></div>"
